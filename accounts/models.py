f26044f14745a681af77e2bb38c2ab7d8cb44d2b
TheoElia <theophylusnhutiphapha@gmail.com>
2020-02-21 06:18:10 +0000

    Create pro api added

--- accounts/models.py
+++ accounts/models.py
@@ -7,12 +7,24 @@ from django.conf import settings
 # Create your models here.
 class CustomUser(AbstractUser):
     # add additional fields in here
+    phone = models.CharField(max_length=14,null=True)
+    userImage = models.ImageField(upload_to="static/profiles",null=True)
+    middle_name = models.CharField(max_length=50,null=True)
     is_staff = models.BooleanField(default=False)
     
 
 
 # Pro model:migrates into database as accounts_pro table
 class Pro(CustomUser):
+    businessName = models.CharField(max_length=50,null=True)
+    scannedId = models.FileField(upload_to="static/IDS",null=True)
+    region = models.CharField(max_length=50,null=True)
+    locationOfService = models.CharField(max_length=50,null=True)
+    address = models.CharField(max_length=120,null=True)
+    id_type = models.CharField(max_length=20,null=True)
+    id_number = models.CharField(max_length=20,null=True)
+    free_comment = models.TextField(null=True)
+    dob = models.DateField(null=True)
     referral_token = models.CharField(max_length=30,null=True,blank=True)
 
     class Meta:
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+import json
+from .models import *
+from random import choice
+
+
+def gen_token(length):
+        token = ''.join([choice('ABCDEFGHIJKLMNOPQabcdefghijklmnopqrstuvwxyz0123456789') for i in range(length)])
+        return token
 
 # Create your views here.
 def index(request):
     template_name = "accounts/index.html"
     args = {}
     return render(request,template_name,args)
+
+
+# This api creates a new pro
+def signup_pro_api(request):
+    # colleting all field from registration of a pro
+    json_data = json.loads(str(request.body, encoding='utf-8'))
+    try:
+        username = json_data['username']
+        password = json_data['password']
+        middleName = json_data['middleName']
+        firstName = json_data['firstName']
+        lastName = json_data['lastName']
+        idType = json_data['idType']
+        idNumber = json_data['idNumber']
+        email = json_data['email']
+        gender = json_data['gender']
+        region = json_data['region']
+        dob = json_data['dob']
+        free_comment = json_data['free_comment']
+        locationOfService = json_data['locationOfService']
+        address = json_data['address']
+        businessName = json_data['businessName']
+        phone = json_data['phone']
+    except Exception as e:
+        data = {'success':False,'message':str(e)}
+    else:
+        # Now initiate creating a new pro
+        try:
+            pro = Pro()
+            pro.username = username
+            pro.set_password(password)
+            pro.first_name = firstName
+            pro.middle_name = middleName
+            pro.last_name = lastName
+            pro.id_type = idType
+            pro.id_number = idNumber
+            pro.region = region
+            pro.locationOfService = locationOfService
+            pro.address = address
+            pro.gender = gender
+            # this must be a date object
+            pro.dob = dob
+            pro.businessName = businessName
+            pro.free_comment = free_comment
+            pro.referral_token = gen_token(25)
+        except Exception as e:
+            data = {'success':False,'message':str(e)}
+        else:
+            pro.save()
+            data = {'success':True,'message':'Pro created successfully'}
+        dump = json.dumps(data)
+        return HttpResponse(dump, content_type='application/json')
+    dump = json.dumps(data)
+    return HttpResponse(dump, content_type='application/json')
+
